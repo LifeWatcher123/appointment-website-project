cef92ed2b879d69c918b911e143148d4d14c398c
Christian Aranas <aranaschristianlouise@gmail.com>
2023-10-17 06:23:14 +0800

    Revert "Added transports attribute"
    This reverts commit bb2b2b0d9e30a2deeafcd5d2fcd9fb54699be132.

--- src-frontend-react/src/socket.js
+++ src-frontend-react/src/socket.js
@@ -1,5 +1,3 @@
 import { io } from 'socket.io-client';
 
-export const socket = io("http://localhost:4000", {
-  autoConnect: false, transports: ['websocket']
-});
+export const socket = io("http://localhost:4000", { autoConnect: false });
