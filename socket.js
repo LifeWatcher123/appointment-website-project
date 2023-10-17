109593a9e100710d6c9ffc7a065503f87e9f9a9d
Christian Aranas <aranaschristianlouise@gmail.com>
2023-10-17 15:35:01 +0800

    Added loggers to socket.js

--- socket.js
+++ socket.js
@@ -1,26 +1,35 @@
 /// Singleton to store a single instance of 
 
 const socketIO = require('socket.io');
+const logger = require('./log');
 
 let ioInstance = null
 
 function initializeSocket(httpServer) {
-  if (!ioInstance) {
-    ioInstance = socketIO(httpServer, {
-      cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-        credentials: true
-      }
-    });
+  try {
+    if (!ioInstance) {
+      ioInstance = socketIO(httpServer, {
+        cors: {
+          origin: "http://localhost:3000",
+          methods: ["GET", "POST"],
+          credentials: true
+        }
+      });
+    }
+  } catch (err) {
+    logger.error(err)
   }
 }
 
 function getSocketInstance() {
-  if (!ioInstance) {
-    throw new Error('Socket.IO instance has not been initialized.');
+  try {
+    if (!ioInstance) {
+      throw new Error('Socket.IO instance has not been initialized.');
+    }
+    return ioInstance;
+  } catch (err) {
+    logger.error(err)
   }
-  return ioInstance;
 }
 
 module.exports = {
